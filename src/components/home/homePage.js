7b4e1c3232a1664d376cdb42263a82ff5e7c8e4b
Yohannes Russom <johnykifle@gmail.com>
2017-10-29 19:39:18 +0100

    add routes and edit existing

---
+++ src/components/home/homePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            <div className="jumbotron">
+                <h1> Plur admin</h1>
+                <p>React, redux rc es6</p>
+                <Link to="about" className="btn btn-primary btn-lg"> Learn more
+                </Link>
+            </div>
+
+        );
+    }
+}
+
+export default HomePage;
